bb88f510a932015ac986e0a690c68ef0ddd0188b
Marcos Ludgerio <raimundo.marcos@dx.ufpb.br>
2024-04-08 08:19:23 -0300

    feat: add new project

---
+++ appPessoas/src/test/java/br/minsait/rmarcosgon/appPessoas/AppPessoasApplicationTests.java
@@ -0,0 +1,13 @@
+package br.minsait.rmarcosgon.appPessoas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppPessoasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
