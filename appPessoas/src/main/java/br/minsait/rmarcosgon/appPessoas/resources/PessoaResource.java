f2213152cb9ca1a6d263b441ee47ded56eca951e
Marcos Ludgerio <raimundo.marcos@dx.ufpb.br>
2024-04-08 09:02:58 -0300

    feat: implemented endereco services

--- appPessoas/src/main/java/br/minsait/rmarcosgon/appPessoas/resources/PessoaResource.java
+++ appPessoas/src/main/java/br/minsait/rmarcosgon/appPessoas/resources/PessoaResource.java
@@ -1,9 +1,13 @@
 package br.minsait.rmarcosgon.appPessoas.resources;
 
+import br.minsait.rmarcosgon.appPessoas.models.Pessoa;
 import br.minsait.rmarcosgon.appPessoas.services.PessoaService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/pessoas")
@@ -12,5 +16,22 @@ public class PessoaResource {
     @Autowired
     PessoaService pessoaService;
 
+    @GetMapping("/")
+    public ResponseEntity<?> getPessoas() {
+        return new ResponseEntity<>(this.pessoaService.listarTodas(), HttpStatus.OK);
+    }
 
+    @PostMapping("/")
+    public ResponseEntity<Pessoa> criarPessoa(@RequestBody Pessoa pessoa) {
+        return new ResponseEntity<>(this.pessoaService.cadastrarPessoa(pessoa), HttpStatus.CREATED);
+    }
+
+    @PutMapping("/")
+    public ResponseEntity<Pessoa> atualizarPessoa(@RequestBody Pessoa pessoa) {
+        try {
+            return new ResponseEntity<>(this.pessoaService.atualizarPessoa(pessoa), HttpStatus.CREATED);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
--- appPessoas/src/main/java/br/minsait/rmarcosgon/appPessoas/services/PessoaService.java
+++ appPessoas/src/main/java/br/minsait/rmarcosgon/appPessoas/services/PessoaService.java
@@ -1,12 +1,45 @@
 package br.minsait.rmarcosgon.appPessoas.services;
 
+import br.minsait.rmarcosgon.appPessoas.models.Pessoa;
 import br.minsait.rmarcosgon.appPessoas.repositories.PessoaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class PessoaService {
 
     @Autowired
     PessoaRepository pessoaRepository;
+
+    public Pessoa cadastrarPessoa(Pessoa pessoa) {
+        return this.pessoaRepository.save(pessoa);
+    }
+
+    public List<Pessoa> listarTodas() {
+        return this.pessoaRepository.findAll();
+    }
+
+    public Optional<Pessoa> pegarPorId(Long id) {
+        return this.pessoaRepository.findById(id);
+    }
+
+    public Pessoa atualizarPessoa(Pessoa pessoa) throws Exception {
+        Optional<Pessoa> pessoaOptional = this.pegarPorId(pessoa.getId());
+        if (pessoaOptional.isEmpty()) throw new Exception("Pessoa não encontrada, verifique o ID!");
+        Pessoa pessoaUpdate = pessoaOptional.get();
+        pessoaUpdate.setDocumento(pessoa.getDocumento());
+        pessoaUpdate.setNome(pessoa.getNome());
+        this.pessoaRepository.save(pessoaUpdate);
+        return pessoaUpdate;
+    }
+
+    public void apagarPessoa(Long id) throws Exception {
+        Optional<Pessoa> pessoaOptional = this.pegarPorId(id);
+        if (pessoaOptional.isEmpty()) throw new Exception("Pessoa não encontrada, verifique o ID!");
+        this.pessoaRepository.delete(pessoaOptional.get());
+    }
+
 }
