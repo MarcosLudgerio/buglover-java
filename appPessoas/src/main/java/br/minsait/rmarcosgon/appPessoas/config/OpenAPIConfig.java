d029f25eaad14369fb5338c421fffb81d9fc5e15
Marcos Ludgerio <raimundo.marcos@dx.ufpb.br>
2024-04-09 08:51:34 -0300

    feat: implemented swagger docs v3

---
+++ appPessoas/src/main/java/br/minsait/rmarcosgon/appPessoas/config/OpenAPIConfig.java
@@ -0,0 +1,41 @@
+package br.minsait.rmarcosgon.appPessoas.config;
+
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.servers.Server;
+import io.swagger.v3.oas.models.OpenAPI;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import io.swagger.v3.oas.models.info.Contact;
+
+import java.util.List;
+
+@Configuration
+public class OpenAPIConfig {
+
+    @Value("${minsait.rmarcos.test-url}")
+    private String testUrl;
+
+    @Bean
+    public OpenAPI myOpenAPI() {
+        Server server = new Server();
+        server.setUrl(testUrl);
+        server.setDescription("Server URL in Localhost");
+
+        Contact contact = new Contact();
+        contact.setEmail("rmarcosgon@minsait.com");
+        contact.setName("Marcos Ludgério");
+        contact.setUrl("https://github.com/marcosludgerio/");
+
+        License mitLicense = new License().name("MIT License").url("https://choosealicense.com/licenses/mit/");
+
+        Info info = new Info()
+                .title("API App Pessoas")
+                .version("1.0")
+                .contact(contact)
+                .description("Esta API cadatros de pessoas.")
+                .license(mitLicense);
+        return new OpenAPI().info(info).servers(List.of(server));
+    }
+}
--- appPessoas/src/main/java/br/minsait/rmarcosgon/appPessoas/resources/PessoaResource.java
+++ appPessoas/src/main/java/br/minsait/rmarcosgon/appPessoas/resources/PessoaResource.java
@@ -5,8 +5,6 @@ import br.minsait.rmarcosgon.appPessoas.dtos.PessoaNomeDto;
 import br.minsait.rmarcosgon.appPessoas.models.Pessoa;
 import br.minsait.rmarcosgon.appPessoas.services.PessoaService;
 import br.minsait.rmarcosgon.appPessoas.services.exceptions.PessoaNotFoundException;
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -16,38 +14,31 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/pessoas")
-@Api(value = "App Pessoa")
-@CrossOrigin(origins = "*")
 public class PessoaResource {
 
     @Autowired
     PessoaService pessoaService;
 
     @GetMapping("/")
-    @ApiOperation(value = "Retorna todas as pessoas cadastradas")
     public ResponseEntity<List<PessoaNomeDto>> getPessoas() {
         return new ResponseEntity<>(this.pessoaService.listarTodas(), HttpStatus.OK);
     }
 
     @GetMapping("/{id}")
-    @ApiOperation(value = "Retorna detalhes de uma única pessoa")
     public ResponseEntity<?> getPessoas(@PathVariable Long id) throws PessoaNotFoundException {
         return new ResponseEntity<>(this.pessoaService.pegarPorId(id), HttpStatus.OK);
     }
 
     @PostMapping("/")
-    @ApiOperation(value = "Cadastra nova pessoa no sistema")
     public ResponseEntity<?> criarPessoa(@RequestBody PessoaDto pessoa) {
         return new ResponseEntity<>(this.pessoaService.cadastrarPessoa(pessoa), HttpStatus.CREATED);
     }
 
     @PutMapping("/")
-    @ApiOperation(value = "Atualiza dados de uma pessoa cadastrada")
     public ResponseEntity<Pessoa> atualizarPessoa(@RequestBody Pessoa pessoa) throws PessoaNotFoundException {
         return new ResponseEntity<>(this.pessoaService.atualizarPessoa(pessoa), HttpStatus.OK);
     }
     @DeleteMapping("/{id}")
-    @ApiOperation(value = "Apaga pessoa pelo id")
     public ResponseEntity<?> apagarPessoa(@PathVariable Long id) throws PessoaNotFoundException {
         this.pessoaService.apagarPessoa(id);
         return ResponseEntity.noContent().build();
